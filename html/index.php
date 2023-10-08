655bc41443743bfed2e281f32758f19f7d7f3f49
Lukas Pastva <lukas.pastva>
2023-10-08 13:02:09 +0200

    feat: GA

--- html/index.php
+++ html/index.php
@@ -77,6 +77,15 @@ $description = str_replace('"','\'',(isDetail()?getDescriptionForClanok($_REQUES
 echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 	<html xmlns="http://www.w3.org/1999/xhtml" xmlns:og="http://opengraphprotocol.org/schema/" xmlns:fb="http://www.facebook.com/2008/fbml" dir="ltr" lang="sk">
 	<head profile="http://gmpg.org/xfn/11">
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-YQ72Q66F80"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-YQ72Q66F80');
+    </script>
 	<meta name="robots" content="index, follow" />
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<meta http-equiv="content-language" content="sk" />
